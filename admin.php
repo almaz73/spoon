200d94fcab4e1baf57b86a8e7da8d3e36a2a1bc9
almaz73 <almaz_73@mail.ru>
2026-03-27 11:23:01 +0700

    php вход в админку по доступу

--- admin.php
+++ admin.php
@@ -1,3 +1,11 @@
+<?php
+$username = "носов";
+$password = "носов";
+$nonsense = "f07854328kazan27032026fag584ghf63h264yh245h5y";
+if (isset($_COOKIE['PrivatePageLogin'])) {
+   if ($_COOKIE['PrivatePageLogin'] == md5($password.$nonsense)) {
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -182,3 +190,35 @@
 <script src="js/lightbox.js" type="text/javascript"></script>
 </body>
 </html>
+
+<?php
+      exit;
+   } else {
+      echo "Bad Cookie.";
+      exit;
+   }
+}
+if (isset($_GET['p']) && $_GET['p'] == "login") {
+   if ($_POST['user'] != $username) {
+      echo "Sorry, that username does not match.";
+      exit;
+   } else if ($_POST['keypass'] != $password) {
+      echo "Sorry, that password does not match.";
+      exit;
+   } else if ($_POST['user'] == $username && $_POST['keypass'] == $password) {
+      setcookie('PrivatePageLogin', md5($_POST['keypass'].$nonsense));
+      header("Location: $_SERVER[PHP_SELF]");
+   } else {
+      echo "Sorry, you could not be logged in at this time.";
+   }
+}
+?>
+<div style="text-align: center">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>?p=login" method="post">
+<br>
+<input type="text" name="user" id="user"/> Логин_ </label><br />
+<label  style="width: 300px;" ><input type="password" name="keypass" id="keypass"  /> Пароль</label><br />
+<br>
+<input type="submit" id="submit" value="Login" />
+</form>
+</div>
